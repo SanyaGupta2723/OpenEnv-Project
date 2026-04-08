fe0a2d6d04247f6326136a1114370b0a35a21c3b
Sanya Gupta <sanyakvds@gmail.com>
2026-04-08 20:25:18 +0530

    asad

--- inference.py
+++ inference.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 API_KEY = os.getenv("AIzaSyBf14w9-7E3DdEiNyfirDITArSWgPZMEyo")
 
-url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key={API_KEY}"
+url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key={API_KEY}"
 
 env = ResumeEnv()
 
